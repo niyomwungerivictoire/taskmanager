e1b5a8d7fea6fb044f400833bb6c636437f51448
niyomwungerivictoire <niyomwungerivictoire177@gmail.com>
2025-04-18 11:44:03 +0300

    added new server running script

--- src/config/env.js
+++ src/config/env.js
@@ -1,9 +1,8 @@
-import dotenv from 'dotenv';
-
+import dotenv from "dotenv"
 dotenv.config();
 export default{
     port: process.env.PORT||5000,
-    mongodburl: process.env.MONGO_URL,
+    mongodburl: process.env.MONGO_URL || "mongodb+srv://niyomwungerivictoire177:1hRDZvtT0yRrBI6x@cluster0.soufodf.mongodb.net/taskmanagement",
     jwtSecret: process.env.JWT_SECRET,
     jwtexpire:process.env.JWT_EXPIRE ||'7d'
 };
